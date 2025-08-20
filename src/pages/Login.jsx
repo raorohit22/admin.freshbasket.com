188f141b6c256b7f529f440f816c16ce5fe48b77
Rohit Yadav <118039098+raorohit22@users.noreply.github.com>
2025-08-20 19:58:39 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
       <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
         <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
           <div className="flex flex-col overflow-y-auto md:flex-row">
-{/*             <div className="h-32 md:h-auto">
+            <div className="h-32 md:h-auto md:w-1/2">
               <img
                 aria-hidden="true"
                 className="object-cover w-full h-full dark:hidden"
@@ -36,8 +36,8 @@ const Login = () => {
                 src={ImageDark}
                 alt="Office"
               />
-            </div> */}
-            <main className="flex items-center justify-center p-6 sm:p-12">
+            </div>
+            <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
               <div className="w-full">
                 <h1 className="mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
                   Login
